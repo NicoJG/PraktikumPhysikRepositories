cdea4f042c7935d60fd1638aa876737265e72477
Nico Guth <nico.guth@tu-dortmund.de>
2020-11-12 19:07:20 +0100

    table updated

--- generate-table.py
+++ generate-table.py
@@ -8,17 +8,18 @@ github_url = 'https://github.com/'
 with open('data.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-        print(row)
         if len(row) == 2 and row[0][0] != '#': 
             user = '['+row[0]+']('+github_url + row[0] + ')'
             repo = '['+row[1]+']('+github_url + row[0] + '/' + row[1] + ')'
             data.append([user,repo])
 
-print(data)
 
 writer = MarkdownTableWriter()
-writer.table_name = "Anfängerpraktikum Repositories"
+#writer.table_name = "Anfängerpraktikum Repositories"
 writer.headers = ["User", "Repository"]
 writer.value_matrix = data
 
 writer.write_table()
+
+with open('table.txt','w') as f:
+    f.write(writer.dumps())
